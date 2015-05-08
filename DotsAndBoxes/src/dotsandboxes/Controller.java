8b9cab92a289287e6a114e9c7a75453ee84dad6c
MarioBucher <mario.b@gmx.ch>
2015-05-08 11:32:52 +0200

    .

--- DotsAndBoxes/src/dotsandboxes/Controller.java
+++ DotsAndBoxes/src/dotsandboxes/Controller.java
@@ -6,6 +6,7 @@
 package dotsandboxes;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
@@ -20,25 +21,38 @@ import javax.swing.JMenuItem;
  */
 public class Controller extends JFrame{
     
-    private JMenu DotsAndBoxes;
-    private JMenuBar file;
+    private JMenu filemenu;
+    private JMenuBar menubar;
     private JMenuItem game; 
     private JLabel label1;
     private JLabel label2;
+    private JPanel panel1;
+    private JPanel panel2;
     
     
     public Controller(){
     
         super("Dots and Boxes");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel1 = new JPanel();
+        setLayout(new BorderLayout());
+        
         label1 = new JLabel("Player 1");
         label2 = new JLabel("Player 2");
-        DotsAndBoxes = new JMenu("File");
-        file = new JMenuBar();
-        game = new JMenuItem("new Game");    
-        setLayout(new BorderLayout());
-        add(DotsAndBoxes);
+        filemenu = new JMenu("File");
+        menubar = new JMenuBar();
+        game = new JMenuItem("new Game");
+        filemenu.add(game);
+        menubar.add(filemenu);
+        setJMenuBar(menubar);
         
+        panel2 = new JPanel();
+        panel2.setLayout(new GridLayout(1,2));
+        panel2.add(label1);
+        panel2.add(label2);
+        add(panel2);
+        
+        setVisible(true);
         
     }
 }
