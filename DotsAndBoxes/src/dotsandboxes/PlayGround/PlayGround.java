a125c67477d7afba4223240c59fa3b16681459df
akauz <adrian.kauz@stud.hslu.ch>
2015-05-15 08:53:30 +0200

    First Methods

--- DotsAndBoxes/src/dotsandboxes/PlayGround/PlayGround.java
+++ DotsAndBoxes/src/dotsandboxes/PlayGround/PlayGround.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dotsandboxes;
+package dotsandboxes.PlayGround;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -22,6 +22,7 @@ public class PlayGround extends JPanel
 {
     
     //Attributes
+    
     
     //Konstruktor
     public PlayGround()
